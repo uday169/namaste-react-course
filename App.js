107ceacf62e11d57b6328cdcd48c4f6f354a1a21
Uday Pratap <udaypratap169@gmail.com>
2023-06-19 02:01:53 +0530

    installed parcel and react

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
-/**
- *  <div id="parent">
- *      <div id= "child">
- *          <h1>I'm h1 tag</h1>
- *      </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
